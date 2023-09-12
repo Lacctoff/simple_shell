772dd06513720a860172e2e00f149feafd7551a2
Lacctoff <wolilacctoff@gmail.com>
2023-09-11 23:21:30 -0700

    shell header files

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+
+
+#endif

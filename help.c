fc29bd450be45a53e38b7a5474beebec8339d482
Lacctoff <wolilacctoff@gmail.com>
2023-09-22 11:59:39 -0700

    correcting myhelp file

--- help.c
+++ help.c
@@ -12,8 +12,8 @@ int _myhelp(info_t *info)
 	char **arg_array;
 
 	arg_array = info->argv;
-	_puts("help call works. Function not yet implemented \n");
+	puts("help call works. Function not yet implemented \n");
 	if (0)
-		_puts(*arg_array); /* temp att_unused workaround */
+		puts(*arg_array);
 	return (0);
 }
